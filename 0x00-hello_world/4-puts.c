a3c585cf614ec67efccb82183b82b2943251d47d
Mjemri <M1.jemri@gmail.com>
2023-07-13 15:43:58 -0700

    pust function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *   main - A program that print a line with pusts
+ *   Return: 0 (Success)
+ */
+int main(void)
+{
+pust("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
