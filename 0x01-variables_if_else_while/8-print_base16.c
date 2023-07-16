be839704d6388b10df216b6c13b7b3f30902fbc8
Mjemri <M1.jemri@gmail.com>
2023-07-16 12:02:48 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  main - A program that print charactere
+ *   Return: 0 (Success)
+*/
+int main(void)
+{
+	char c;
+	int i;
+
+for (i = 0; i < 10; i++)
+putchar('0' + i);
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
