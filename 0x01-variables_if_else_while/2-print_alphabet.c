a708c4a592a436e15424c5dcf7fd1918cdd83908
Mjemri <136837365+Mjemri@users.noreply.github.com>
2023-07-16 18:51:18 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *  main - A program that print last digit
+ *   Return: 0 (Success)
+ */
+int main() {
+  char c;
+  
+for (c = 'a'; letter <= 'z'; c++) 
+    putchar(c);
+putchar('\n');
+return 0;
+}
