6f7fdd53dd4c634589d19d6a3ff8663f9f1a1a82
Mjemri <M1.jemri@gmail.com>
2023-07-16 11:30:31 -0700

    number 0 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *  main - A program that print number from 0 to 9
+ *   Return: 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+for (c = 0; c < 10; c++)
+printf("%d", c);
+printf("\n");
+return (0);
+}
